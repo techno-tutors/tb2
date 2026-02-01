b5395bc2138645991958857231e0669afb7f3468
f._. <ists821@gmail.com>
2026-02-01 12:07:17 +0000

    simple config wrote #5

---
+++ script/subcmds/config.d/config.list.sh
@@ -0,0 +1,8 @@
+# organization name
+ORG_NAME=""
+# your github user name
+USR_NAME=""
+# direct edit flag is when you create new page and write it, tb2 will add the md file into directory.
+# then if this is true, you should where's the new file.
+# else if this is false, tb2 will make a directory "DRAFT_PAGES" to your computer, and you will open it easily.
+DIRECT_EDIT=false
